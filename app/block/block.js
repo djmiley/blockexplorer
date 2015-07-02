6ec121715518773779bea0ce8c675f55672b1a8f
Drew Miley <dmiley@scottlogic.co.uk>
2015-07-02 10:22:41 +0100

    Block info page implemented

--- app/block/block.js
+++ app/block/block.js
@@ -3,19 +3,19 @@
 angular.module('myApp.block', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/block', {
+  $routeProvider.when('/block/:blockHash*', {
     templateUrl: 'block/block.html',
     controller: 'BlockCtrl'
   });
 }])
 
-.controller('BlockCtrl', [function() {
+.controller('BlockCtrl', ['$scope', '$http', '$routeParams', '$sce', function($scope, $http, $routeParams, $sce) {
 	
-	$scope.lastTenBlocks = [];
+	$scope.blockHash = $sce.trustAsResourceUrl($routeParams.blockHash);
 	
 	$scope.latestBlock = [];
 	
-    $http.get("http://localhost:8080/blockchain/latestblock")
+    $http.get("http://localhost:8080/blockchain/rawblock/" + $scope.blockHash)
     .success(function (data, status, headers, config) {
 	  $scope.latestBlock = data;
 			
