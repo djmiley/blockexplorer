248e5091a51c9f201483ca5087119b2d60411342
Drew Miley <dmiley@scottlogic.co.uk>
2015-07-06 14:27:10 +0100

    Working copy

--- app/block/block.js
+++ app/block/block.js
@@ -20,14 +20,7 @@ angular.module('myApp.block', ['ngRoute'])
 	  $scope.block = data;
 	  
 	  $scope.transactionSizes = [];
-	  
-	  $scope.myData = [
-    {name: 'AngularJS', count: 300},
-    {name: 'D3.JS', count: 150},
-    {name: 'jQuery', count: 400},
-    {name: 'Backbone.js', count: 300},
-    {name: 'Ember.js', count: 100}
-];
+
 	  var transaction;
 	  
 	  for (var i = 0; i < $scope.block.tx[i].size; i++) {
